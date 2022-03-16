25800ac04a0f658dd816f882b2bd72f3b60f2457
MaxFlamesa <dasha.moose1967@gmail.com>
2022-03-16 14:24:13 +0300

    orm

--- not so simple site/new_file.py
+++ not so simple site/new_file.py
@@ -1,3 +1,4 @@
+from orm import the_user, category, product, favorites, orders, order_comp
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,10 +12,20 @@ def mainnn():
 
 @app.route('/shirts')
 def cat_shrt():
-    return render_template("katalog.html", items=[{'name': "футболка 'Правило'", 'image': "cakehole.jpg", 'price': "2005 руб", 'description': "или за касету Металлики/Нирваны/Моторхеда"}, {'name': "футболка 'Импала'", 'image': "impala.jpg", 'price': "1967 руб", 'description': "Не надо лишних слов: KAZ2Y5"}])
+    return render_template("katalog.html", items=category.query())
 
 @app.route('/drinks')
 def cat_drink():
-    return render_template("katalog.html", items=[{'name': "Медовый лимонад от Бобби", 'image': "honeyy.png", 'price': "от 80 руб", 'description': "Никого - кроме аллергиков - не оставит равнодушным"}, {'name': "Имбирно-пряный лимонад от Бобби", 'image': "ginger.png", 'price': "от 80 руб", 'description': "Никого - кроме аллергиков - не оставит равнодушным"}, {'name': "Цитрусовый лимонад от Бобби", 'image': "citruus.png", 'price': "от 80 руб", 'description': "Никого - кроме аллергиков - не оставит равнодушным"}])
+    return render_template("katalog.html", items=[{'name': "Медовый лимонад от Бобби", 'image': "honeyy.png", 'price': "от 80 руб", 'description': "Никого - кроме аллергиков - не оставит равнодушным"}
+    , {'name': "Имбирно-пряный лимонад от Бобби", 'image': "ginger.png", 'price': "от 80 руб", 'description': "Никого - кроме аллергиков - не оставит равнодушным"}, 
+    {'name': "Цитрусовый лимонад от Бобби", 'image': "citruus.png", 'price': "от 80 руб", 'description': "Никого - кроме аллергиков - не оставит равнодушным"}])
+
+@app.route('/lemfru')
+def nap_fru():
+    return render_template("tovar.html")
+
+@app.route('/about')
+def mee():
+    return render_template("me.html")
 
 app.run(debug=True)
